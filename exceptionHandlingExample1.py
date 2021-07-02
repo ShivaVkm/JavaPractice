e48ece45a4ed8cdba6063684893fd3cb1a4f68d4
ShivaVkm <shivavkm2001@gmail.com>
2021-07-02 22:54:43 +0530

    exceptionHandlingExample1.py file gets committed here

---
+++ exceptionHandlingExample1.py
@@ -0,0 +1,6 @@
+codingYouTubeChannels = ['Telusko', 'Hitesh Choudhary', 'CodeWithHarry', 'MySirG', 'Anuj Bhaiya', 'Apna College']
+try:
+    print("1st channel in the list is %s"%(codingYouTubeChannels[0]))
+    print("7th channel in the list is %s"%(codingYouTubeChannels[6]))
+except:
+    print("An index error occured")
